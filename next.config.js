1d85e7a4bdeee33c635ff20909d67aef4d486f98
Aviv Kandabi <kandabiaviv@gmail.com>
2022-12-19 11:40:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag|ps)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    config.module.rules.push({
+        test: /\.(glb|gltf|mp3|wav|)$/,
+        type: 'asset/resource',
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
